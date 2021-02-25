63ba300cba6d44f8feeec3577c5974ca6dace651
ChicaizaJefferson <79608495+ChicaizaJefferson@users.noreply.github.com>
2021-02-24 20:41:45 -0500

    Add files via upload

---
+++ HerenciaChicaizaJefferson/Examen Practico/Ejercicio/ejercicio/frmportada.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace ejercicio
+{
+    partial class frmportada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gboportada = new System.Windows.Forms.GroupBox();
+            this.btncliente = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.btnempleado = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gboportada.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gboportada
+            // 
+            this.gboportada.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.gboportada.Controls.Add(this.label4);
+            this.gboportada.Controls.Add(this.label3);
+            this.gboportada.Controls.Add(this.label2);
+            this.gboportada.Controls.Add(this.label1);
+            this.gboportada.Controls.Add(this.btncliente);
+            this.gboportada.Controls.Add(this.btnsalir);
+            this.gboportada.Controls.Add(this.btnempleado);
+            this.gboportada.Location = new System.Drawing.Point(16, 146);
+            this.gboportada.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gboportada.Name = "gboportada";
+            this.gboportada.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gboportada.Size = new System.Drawing.Size(1035, 337);
+            this.gboportada.TabIndex = 6;
+            this.gboportada.TabStop = false;
+            this.gboportada.Text = "portada";
+            // 
+            // btncliente
+            // 
+            this.btncliente.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncliente.Location = new System.Drawing.Point(421, 240);
+            this.btncliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btncliente.Name = "btncliente";
+            this.btncliente.Size = new System.Drawing.Size(154, 42);
+            this.btncliente.TabIndex = 2;
+            this.btncliente.Text = "Cliente";
+            this.btncliente.UseVisualStyleBackColor = true;
+            this.btncliente.Click += new System.EventHandler(this.btncliente_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalir.Location = new System.Drawing.Point(868, 252);
+            this.btnsalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(141, 60);
+            this.btnsalir.TabIndex = 1;
+            this.btnsalir.Text = "salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // btnempleado
+            // 
+            this.btnempleado.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnempleado.Location = new System.Drawing.Point(421, 137);
+            this.btnempleado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnempleado.Name = "btnempleado";
+            this.btnempleado.Size = new System.Drawing.Size(154, 42);
+            this.btnempleado.TabIndex = 0;
+            this.btnempleado.Text = "Empleado";
+            this.btnempleado.UseVisualStyleBackColor = true;
+            this.btnempleado.Click += new System.EventHandler(this.btnempleado_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(30, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "EXAMEN SEGUNDO PARCIAL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(78, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 72);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PROGRAMACION \r\n         VIZUAL\r\n           4101";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(248, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(552, 28);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "UNIVERCICAD DE LAS FUERZAS ARMADAS ESPE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(68, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(218, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "CHICAIZA JEFFERSON";
+            // 
+            // frmportada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = global::ejercicio.Properties.Resources.descarga__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.gboportada);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmportada";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmportada";
+            this.Load += new System.EventHandler(this.frmportada_Load);
+            this.gboportada.ResumeLayout(false);
+            this.gboportada.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gboportada;
+        private System.Windows.Forms.Button btncliente;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.Button btnempleado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
